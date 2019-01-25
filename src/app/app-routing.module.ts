0aa902213ad6ddf9f0d47eb8393248b85cea88d9
Fernanda Moreira <fernanda.moreira@viasoft.com.br>
2019-01-25 16:52:09 -0200

    Created base app configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {MainComponent} from "./home/main/main.component";
+import {SigninComponent} from "./home/signin/signin.component";
+import {SignupComponent} from "./home/signup/signup.component";
+import {LoginGuard} from "./core/auth/login.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    canActivate: [LoginGuard],
+    children: [
+      {
+        path: "",
+        component: MainComponent,
+        data: {
+          title: "myMoney"
+        }
+      },
+      {
+        path: "signin",
+        component: SigninComponent,
+        data: {
+          title: "Sign in"
+        }
+      },
+      {
+        path: "signup",
+        component: SignupComponent,
+        data: {
+          title: "Sign up"
+        }
+      }
+    ]
+  },
+  {
+    path: "admin",
+    loadChildren: "./admin/administration.module#AdministrationModule"
+  },
+  {
+    path: "user",
+    loadChildren: "./user/user.module#UserModule"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import {RouterModule} from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeModule } from "./home/home.module";
+import {CoreModule} from "./core/core.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    CoreModule,
+    HomeModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
